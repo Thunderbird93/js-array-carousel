94e5cfd8e4e8e1b4679fd0a9ab14a39528f1d2f9
Thunderbird93 <antircristian@gmail.com>
2022-10-17 21:10:55 +0200

    fix js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,24 @@
-const images [
+const imagesArray = [
     '01.jpg',
-    '02.jpg',
-    '03.jpg',
-    '04.jpg',
-    '05.jpg',
+    '02.jgp', 
+    '03.jpg', 
+    '04.jpg', 
+    '05.jpg'
 ];
 
-console.log(images);
+let imagesTags = '';
+
+const slider = document.querySelector('.slider');
+
+for (let i = 0; i < imagesArray.length; i++){
+    imagesTags += `
+        <img class="item" src="img/${imagesArray[i]}" alt="${imagesArray[i]}">
+    `;
+}
+
+let counterImages = 0;
+
+const up = document.querySelector('.up');
+const down = document.querySelector('.down');
+
+slider.innerHTML += imagesTags;
